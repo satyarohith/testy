93e78102950fffa3bb3f3215a60c9fa33d096182
Satya Rohith <me@satyarohith.com>
2021-03-01 21:39:24 +0530

    pushing the feature to main

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { json, serve } from "https://deno.land/x/sift@0.1.4/mod.ts";
 
 serve({
-  "/": () => json({ message: "really cool api" }),
+  "/": () => {
+    return json({ message: "really cool api", date: new Date().toString() });
+  }
 });
