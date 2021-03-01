4beaa8b4fd16b8296c8b70d0774c7e75796a4191
Satya Rohith <me@satyarohith.com>
2021-03-01 21:38:14 +0530

    another new featueer

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { json, serve } from "https://deno.land/x/sift@0.1.4/mod.ts";
 
 serve({
-  "/": () => json({ message: "really cool api" }),
+  "/": () =>
+    json({ message: "really cool api", date: new Date().toLocaleDateString() }),
 });
